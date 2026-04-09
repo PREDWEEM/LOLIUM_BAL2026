aa5ee97d6ba617969488ba764c05a0cb4d8cd3df
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-04-09 11:46:12 -0300

    Update valida2.py

--- valida2.py
+++ valida2.py
@@ -512,7 +512,7 @@ umbral_termoinhibicion = st.sidebar.number_input(
 st.sidebar.markdown("**Ruptura de Dormición (Otoño Temprano)**")
 umbral_choque_hidrico = st.sidebar.slider(
     "Choque Hídrico 3 días (mm)", 
-    min_value=10.0, max_value=100.0, value=10.0, 
+    min_value=10.0, max_value=100.0, value=30.0, 
     help="Desbloquea la emergencia temprana si se acumula esta lluvia antes de fines de abril."
 )
 
